0f59ee03c7aacfd3290c45a3a51419a875e72865
Divided by Zer0 <mail@dbzer0.com>
2014-09-18 22:01:40 +0200

    Revert "shutdown"
    This reverts commit 73d555abace95366715b47ff431c1b1d8ad86fbe.

--- src/Netrunnerdb/BuilderBundle/Controller/SocialController.php
+++ src/Netrunnerdb/BuilderBundle/Controller/SocialController.php
@@ -1260,8 +1260,6 @@ class SocialController extends Controller
         $response->setPublic();
         $response->setMaxAge($this->container->getParameter('short_cache'));
         
-        return $this->render('NetrunnerdbBuilderBundle:Default:shutdown.html.twig');
-        
         $decklists_recent = $this->recent(0, 10)['decklists'];
         
         $dbh = $this->get('doctrine')->getConnection();
